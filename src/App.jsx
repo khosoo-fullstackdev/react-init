9a87aa4432ca4b77136274556215db1b2dcbe675
Angi <khsoo.fsdev@gmail.com>
2024-01-09 09:23:51 +0800

    wefw

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Cards from "./components/Cards";
 import Highlights from "./components/Highlights";
-import Header from "./components/Header";
 export default function App() {
   return (
     <div className="top">
